26337b290c5e06b826de3bf482d17a1cba0ac3b8
Thomas Topfstedt <thotop100@gmail.com>
2021-04-06 19:39:33 -0300

    perl

--- MOSS/Grupos/run.bat
+++ MOSS/Grupos/run.bat
@@ -1,10 +1,8 @@
 @echo
-IF EXIST //C:Perl64/ GOTO :start
-ELSE
-msiexec /i bin/ActivePerl-5.28.msi
-
 :start
+::============
 perl moss.perl -l c -d brasiliaalex/exec.asm thomashenrique/exec.asm
+::=============
 SET /p environment="Rodar novamente? (1) Fechar? (0)"
 IF /i "%environment%" == "1" GOTO start
 IF /i "%environment%" == "0" GOTO end
