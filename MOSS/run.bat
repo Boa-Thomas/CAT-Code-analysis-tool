1896c2319d31af1ea06e794af1be417df19fb4f6
Thomas Topfstedt <thotop100@gmail.com>
2021-04-06 19:43:41 -0300

    1

--- MOSS/run.bat
+++ MOSS/run.bat
@@ -1,6 +1,8 @@
 @echo
 :start
-perl moss.perl -l c -d test/brasilia/exec.asm test/thomas/exec.asm test/vini/exec.asm test/alek/exec.asm test/henrique/exec.asm test/jorge/exec.asm
+::============
+perl moss.perl -l c -d brasiliaalex/exec.asm thomashenrique/exec.asm
+::=============
 SET /p environment="Rodar novamente? (1) Fechar? (0)"
 IF /i "%environment%" == "1" GOTO start
 IF /i "%environment%" == "0" GOTO end
